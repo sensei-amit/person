a7b8c7d8d1880f7c425c028c3171839b157bf8b5
Amit <164203799+sensei-amit@users.noreply.github.com>
2025-09-06 09:46:13 +0530

    Delete Persondetails.java

--- Persondetails.java
+++
@@ -1,9 +0,0 @@
-public class Persondetails {
-    public static void main(String[] args) {
-        // Creating an object and providing details
-        Person person1 = new Person("Amit", 25, "Hubballi");
-        
-        // Displaying the details
-        person1.displayDetails();
-    }
-}
